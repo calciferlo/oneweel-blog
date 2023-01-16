83ad3c9d891fb1b1e034c752b9775871e3fd344c
Damian Janus <damian.janus@gmail.com>
2023-01-16 14:14:47 +0100

    repair ts error

--- app/session.server.ts
+++ app/session.server.ts
@@ -3,6 +3,7 @@ import invariant from "tiny-invariant";
 
 import type { User } from "~/models/user.server";
 import { getUserById } from "~/models/user.server";
+import "./env.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
@@ -65,7 +66,7 @@ export async function requireUser(request: Request) {
 
 export async function requireAdminUser(request: Request) {
   const user = await requireUser(request);
-  if (user.email !== ENV.ADMIN_EMAIL) {
+  if (user?.email !== ENV.ADMIN_EMAIL) {
     throw await logout(request);
   }
   return user;
